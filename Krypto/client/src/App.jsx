c5fd5dd06737f3139e0ed7459acfb0730bd58fc2
Athul3Shaji <athulshaji125@gmail.com>
2022-11-24 17:27:53 +0530

    crypto wallet

---
+++ Krypto/client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Services,Welcome,Footer,Transactions,Navbar } from "./components"
+
+const App =   () =>{
+  
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+      <Navbar/>
+        <Welcome/>
+        
+
+      </div>
+      <Services/>
+      <Transactions/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Krypto/client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from'./Transactions.json';
+
+
+export const contractABI=abi.abi;
+export const contractAddress ='0x880D37FbF200e68b4167Bd5B2f31c1ecbffC0c42';
