4f2941fbb3c724f78c9846cc73bdaa235e24cedd
Athul3Shaji <110802380+Athul3Shaji@users.noreply.github.com>
2022-11-29 09:20:42 +0530

    Update hardhat.config.js

--- Krypto/smart_contract/hardhat.config.js
+++ Krypto/smart_contract/hardhat.config.js
@@ -5,8 +5,8 @@ module.exports ={
   solidity: '0.8.0',
   networks: {
     goerli:{
-      url:'https://eth-goerli.g.alchemy.com/v2/WW527X5FH9FE_HwNqFjEFYGB-_G_cjE3 ',
-      accounts:['7fd91a5e4d31cb00bcd52fb6d97e4fe6705401a06effe51bbdf2a40180e2ff2d']
+      url:'',
+      accounts:['']
     }
   }
 } 
